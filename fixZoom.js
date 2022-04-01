d2743321be39a87d459734538a3a3423034fa7f1
acarrabbav <acarrabba@verily.com>
2022-03-31 17:45:55 -0700

    no longer change style.zoom

--- fixZoom.js
+++ fixZoom.js
@@ -1,18 +1,13 @@
 function updateZoom(tabId) {
     chrome.tabs.setZoomSettings(
-        tabId, { mode: "disabled", scope: "per-tab" },
+        tabId, { mode: "automatic", scope: "per-tab" },
         _ => console.log(chrome.runtime.lastError)
     );
 
     chrome.tabs.getZoom(tabId, (zoomFactor) => {
-        chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            func: (zoomFactor) => {
-                const newZoomLevel = 1 / +zoomFactor;
-                document.body.style.zoom = newZoomLevel;
-            },
-            args: ["" + zoomFactor]
-        }, _ => console.log(chrome.runtime.lastError));
+        if (zoomFactor !== 1) {
+            chrome.tabs.setZoom(tabId, 1, () => {});
+        }
     });
 }
 
